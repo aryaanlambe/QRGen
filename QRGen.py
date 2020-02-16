7508c199554431f9484bf1ca8d09986d5c5363b1
Aryaan Lambe <lambearyaan@gmail.com>
2020-02-16 20:10:16 +0200

    Added QR generating Python script

---
+++ QRGen.py
@@ -0,0 +1,25 @@
+import io
+import pyqrcode
+from base64 import b64encode
+import eel
+
+eel.init('web')
+
+# Eel launch requirements.
+@ eel.expose
+def dummy (dummy_param):
+    print("I got a parameter: ", dummy_param)
+    return "string_value", 1, 1.2, True, [1, 2, 3, 4], {"name": "eel"}
+
+# Full generate QR Code feature.
+@ eel.expose
+def generate_qr(data):
+    img = pyqrcode.create(data)
+    buffers = io.BytesIO()
+    img.png(buffers, scale=8)
+    encoded = b64encode(buffers.getValue()).decode("ascii")
+    print("QR Code successfully generated")
+    return "data:image/png;base64, " + encoded
+
+# Launch as app, with dimensions: 1000 x 600
+eel.start('index.html', size = (1000, 600))
